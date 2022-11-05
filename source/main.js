7104cfa5c94f673c2f9992f14086a21e34afe6e5
dkpoint <dkpoint.oh@gmail.com>
2022-11-05 18:38:01 +0900

    Feat: Display items

---
+++ source/main.js
@@ -0,0 +1,35 @@
+/*
+    @ main.js
+        # Reference
+            + Using the Fetch API : https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+*/
+
+// Global fetch() method
+function loadItems() {
+  return fetch('data/data.json')
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+function displayItems(items) {
+  const containerItems = document.querySelector('.items');
+  containerItems.innerHTML = items
+    .map((item) => createHTMLString(item))
+    .join('');
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+  `;
+}
+
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    // setEventListeners(items);
+  })
+  .catch(console.log);
